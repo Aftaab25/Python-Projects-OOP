1c9bf4b111d2f1098e69b4d9751f5af0ac6c8ae6
Aftaab Siddiqui <aftaab2507@gmail.com>
2022-11-29 01:02:40 +0530

    implemented pay method

--- App2-Flatmates-Bill/main.py
+++ App2-Flatmates-Bill/main.py
@@ -17,8 +17,9 @@ class Flatmate:
         self.name = name
         self.days_in_house = days_in_house
 
-    def pays(self, bill):
-        pass
+    def pays(self, bill, flatmate2):
+        return (self.days_in_house / (self.days_in_house + flatmate2.days_in_house)) * bill.amount
+        
 
 
 class PdfReport:
@@ -31,3 +32,12 @@ class PdfReport:
 
     def generatePdf(self, flatmate1, flatmate2, bill):
         pass
+
+
+the_bill = Bill(120, "March 2021")
+john = Flatmate(name="John", days_in_house=20)
+marry = Flatmate(name="Marry", days_in_house=25)
+
+print("John pays: ", john.pays(bill=the_bill, flatmate2=marry))
+print("Marry pays: ", marry.pays(bill=the_bill, flatmate2=john))
+# print(john.pays(bill=the_bill, flatmate2=marry) + marry.pays(bill=the_bill, flatmate2=john))
